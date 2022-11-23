4ebd0367dbf9b6b90120aa6dd49b6f17fe80603f
Mikel Arina Marcos <mikel.arina@estudiantat.upc.edu>
2022-11-23 11:19:03 +0100

    login.js completo

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,7 @@
+var BASE_URI="http://147.83.7.208:8080/dsaApp"
+
+//Comenzamos con el login
+
 $(document).ready(function (){
     $("#loginButton").click(function(){
         var username = $('#username').value();
@@ -7,5 +11,29 @@ $(document).ready(function (){
         var user = {"username": username, "password": password};
         console.log(user);
 
-    })
-})
+        $.ajax({
+            type: 'POST',
+            url: BASE_URI.concat("/auth/login"),
+            headers: { 'content-type': 'application/json',"x-kii-appid": "XXXXX","x-kii-appkey":"XXXXX" },
+            data: JSON.stringify(user),
+            dataType: 'json',
+            success: function (data){
+                console.log("Se ha iniciado sesión correctamente");
+                console.log(data);
+                console.log(url);
+                var username = data.username;
+                var name = data.name;
+                // Cogemos los siguientes datos del usuario.
+                window.sessionStorage.setItem("username", username);
+                window.sessionStorage.setItem("name", name);
+                alert('Inicio de sesión correcto');
+                // Llevamos al usuario a la página de información de su perfil
+                var url = "http://147.83.7.208:8080/html/myprofile.html";
+                window.open(url, "_self");
+            },
+            error: function(error){
+                console.log(error.message);
+            }
+        });
+    });
+});
