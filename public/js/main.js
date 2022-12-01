efc306ef2dd27b4cdc14f8893c0f51d48402ccb4
marioRelajao <101522382+marioRelajao@users.noreply.github.com>
2022-12-01 13:32:33 +0100

    Updated main.js to match swagger

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     var $users = $('#users');
     $.ajax({
         type: 'GET',
-        url: BASE_URI.concat("/users/getUsers"),
+        url: BASE_URI.concat("/juego/users"),
         success: function(users){
             $.each(users, function (i, user){
                 $users.append('<li>username: '+user.username+' mail: '+user.mail+' name: '+user.name+' lastname: '+user.lastname+' </li>');
