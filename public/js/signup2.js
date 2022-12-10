5a71b196f4a0af5dcaf44d996212e14774f9e7b2
Mikel Arina Marcos <mikel.arina@estudiantat.upc.edu>
2022-12-10 21:20:48 +0100

    cambios en gignup2.js

--- public/js/signup2.js
+++ public/js/signup2.js
@@ -5,12 +5,21 @@
 // Accedemos a la función que se ejecuta al pulsar el botón de "Registro" en el formulario.
 function registro(){
     // Nos creamos variables para recoger los datos del formulario.
+    alert("Hola");
     var name = $('#name').val();
+    console.log(name);
     var password = $('#password').val();
+    console.log(password);
     var mail = $('#mail').val();
+    console.log(mail);
     var username = $('#name').val();
+    console.log(username);
     var lastName = $('#lastName').val();
+    console.log(lastName);
+    var dinero = 0;
+    console.log(dinero);
 
+    // Creamos el nuevo usuario
     $.post({
         url: '/dsaApp/juego/users/register',
         data: JSON.stringify({
@@ -18,10 +27,12 @@ function registro(){
             "password": password,
             "mail": mail,
             "username": username,
-            "lastName": lastName
+            "lastName": lastName,
+            "dinero": dinero
         }),
         contentType: 'application/json; charset=utf8'
     })
+        // Comprobamos si se ha podido crear y mostramos mensaje con el resultado.
         .done(function (data, status){
             alert("Usuario registrado correctamente.");
             window.location = "../html/informacion.html";
