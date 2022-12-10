994ba7a0a94972bfd2813d6db08fc2be9149746d
Mikel Arina Marcos <mikel.arina@estudiantat.upc.edu>
2022-12-10 10:08:37 +0100

    Cambios clase 05

--- public/js/login2.js
+++ public/js/login2.js
@@ -1,20 +1,20 @@
-$(document).ready(function(){
+function inicioSesion() {
     var $users = $('#users');
-    var username = $('#username');
-    var password = $('#password');
+    var $mail = $('#mail');
+    var $password = $('#password');
     $.ajax({
         contentType: "application/json",
         type: 'POST',
-        url:'/dsaApp/login',
-        data: JSON.stringify({"username": username, "password": password}),
-        success: function(users){
-            alert("Welcome, " + username);
+        url: '/dsaApp/juego/login',
+        data: JSON.stringify({"mail": $mail, "password": $password}),
+        success: function (users) {
+            alert("Welcome, " + mail);
         },
-        error: function (error){
-            if (username == "" || password == "")
+        error: function (error) {
+            if ($mail == "" || $password == "")
                 alert("Tienes que completar todos los campos.");
             else
                 alert("Datos incorrectos.");
         }
     });
-});
+}
--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,7 @@ $(document).ready(function(){
     var $users = $('#users');
     $.ajax({
         type: 'GET',
-        url:'/dsaApp/juego',
-        url: BASE_URI.concat("/juego/users"),
+        url:'/dsaApp/juego/users',
         success: function(users){
             $.each(users, function (i, user){
                 $users.append('<li>username: '+user.username+' mail: '+user.mail+' name: '+user.name+' lastname: '+user.lastname+' </li>');
