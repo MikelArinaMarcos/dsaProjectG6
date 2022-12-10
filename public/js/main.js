bd13552e4c723f63461997c74568a1c6dc1ff398
Mikel Arina Marcos <mikel.arina@estudiantat.upc.edu>
2022-12-10 17:47:53 +0100

    Cambios en main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,1 @@
-$(document).ready(function(){
-    var $users = $('#users');
-    $.ajax({
-        type: 'GET',
-        url:'/dsaApp/juego',
-        url: BASE_URI.concat("/juego/users"),
-        success: function(users){
-            $.each(users, function (i, user){
-                $users.append('<li>username: '+user.username+' mail: '+user.mail+' name: '+user.name+' lastname: '+user.lastname+' </li>');
-            });
-        },
-        error: function (){
-            alert('Se produjo un error cargando los usuarios.');
-        }
-    });
-});
+
