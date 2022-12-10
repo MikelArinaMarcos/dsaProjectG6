159e56147681f255901a6701cfbb972deac4bf47
Mikel Arina Marcos <mikel.arina@estudiantat.upc.edu>
2022-12-10 18:23:04 +0100

    Login web finalizado

--- public/js/login.js
+++ public/js/login.js
@@ -1,37 +1,17 @@
-var BASE_URI="http://147.83.7.208:8080/dsaApp";
-
-$(document).ready(function (){
-    $("#loginButton").click(function(){
-        var username = $('#username').value();
-        console.log(username);
-        var password = $('#password').value();
-        console.log(password);
-        var user = {"username": username, "password": password};
-        console.log(user);
-
-        $.ajax({
-            type: 'POST',
-            url: BASE_URI.concat("/auth/login"),
-            headers: { 'content-type': 'application/json',"x-kii-appid": "XXXXX","x-kii-appkey":"XXXXX" },
-            data: JSON.stringify(user),
-            dataType: 'json',
-            success: function (data){
-                console.log("Se ha iniciado sesión correctamente");
-                console.log(data);
-                console.log(url);
-                var username = data.username;
-                var name = data.name;
-                // Cogemos los siguientes datos del usuario.
-                window.sessionStorage.setItem("username", username);
-                window.sessionStorage.setItem("name", name);
-                alert('Inicio de sesión correcto');
-                // Llevamos al usuario a la página de información de su perfil
-                var url = "http://147.83.7.208:8080/html/myprofile.html";
-                window.open(url, "_self");
-            },
-            error: function(error){
-                console.log(error.message);
-            }
-        });
-    });
-});
+function inicioSesion(){
+    var mail = $('#mail').val();
+    var password = $('#password').val();
+    $.post({
+        url: '/dsaApp/juego/login',
+        data: JSON.stringify({"mail": mail, "password": password}),
+        contentType: 'application/json; charset=utf8'
+    })
+        .done(function (data, status){
+            alert("Welcome");
+            window.location = "../html/informacion.html";
+        })
+        .fail(function(xhr, err){
+            alert("Usuario o contraseña incorrectos");
+            location.reload();
+        })
+};
