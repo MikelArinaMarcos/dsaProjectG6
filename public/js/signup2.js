7738a0c2dc32bd2ee8a46e6c3d31b0deac8f58ca
Mikel Arina Marcos <mikel.arina@estudiantat.upc.edu>
2022-12-10 21:05:57 +0100

    singup2.js editado

--- public/js/signup2.js
+++ public/js/signup2.js
@@ -1,20 +1,33 @@
-function registro(){
-    var $username = $('#username').value();
-    var $password = $('#password').value();
-    var $mail = $('#mail').value();
-    var $name = $('#name').value();
-    var $lastname = $('#lastname').value();
+/******************************************/
+/* JAVASCRIPT PARA EL PROCESO DE REGISTRO */
+/******************************************/
 
-    $.ajax({
-        contentType: "application/json",
-        type: 'POST',
-        url:'/dsaApp/juego/signup',
-        data: JSON.stringify({"username": username, "password": password, "mail": mail, "name": name, "lastname": lastname}),
-        success: function(users){
-            alert("Usuario creado con éxito");
-        },
-        error: function (error){
-            alert("Error al crear el usuario");
-        }
-    });
+// Accedemos a la función que se ejecuta al pulsar el botón de "Registro" en el formulario.
+function registro(){
+    // Nos creamos variables para recoger los datos del formulario.
+    var name = $('#name').val();
+    var password = $('#password').val();
+    var mail = $('#mail').val();
+    var username = $('#name').val();
+    var lastName = $('#lastName').val();
+
+    $.post({
+        url: '/dsaApp/juego/users/register',
+        data: JSON.stringify({
+            "name": name,
+            "password": password,
+            "mail": mail,
+            "username": username,
+            "lastName": lastName
+        }),
+        contentType: 'application/json; charset=utf8'
+    })
+        .done(function (data, status){
+            alert("Usuario registrado correctamente.");
+            window.location = "../html/informacion.html";
+        })
+        .fail(function(xhr, err){
+            alert("Por favor, revisa los datos introducidos");
+            location.reload();
+        })
 };
